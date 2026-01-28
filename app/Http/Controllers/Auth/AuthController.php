985845ff93a608a83ffe280cfefdd925e8deef12
rolofran <99774170+rolofran@users.noreply.github.com>
2026-01-28 09:41:53 -0300

    corrección en logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->usuario()->currentAccessToken()->delete();
+        $request->user()->currentAccessToken()->delete();
 
         return response()->json([
             'message' => 'Sesión cerrada correctamente'
